616018b74e40c629c2879df7bec5f42648728a22
Aurélie PREAUD <aureliepreaud@MacBook-Air-de-Aurelie.local>
2021-04-24 14:44:19 +0200

    envoie du message par mail

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,28 +9,34 @@ $.addEventListener("DOMContentLoaded", () => {
     $.querySelector(".modal").classList.add("display");
   });
 
+  // Bouton croix : ferme le formulaire modal
+
   $.querySelector("#btn-close").addEventListener("click", () => {
     $.querySelector(".modal").classList.remove("display");
   });
 
-  // Envoyer les données du formulaire
+  // Bouton Envoyer les données du formulaire
 
   $.querySelector("#contact-form").addEventListener("submit", async (event) => {
-    event.preventDefault();
+    // event.preventDefault();
 
-    const data = {
-      firstname: $.querySelector("#firstname").value,
-      lastname: $.querySelector("#lastname").value,
-      email: $.querySelector("#email").value,
-      message: $.querySelector("#message").value,
-    };
+    if (firstname && lastname && email && message) {
+      const data = {
+        firstname: $.querySelector("#firstname").value,
+        lastname: $.querySelector("#lastname").value,
+        email: $.querySelector("#email").value,
+        message: $.querySelector("#message").value,
+      };
 
-    console.log(data);
+      console.log(data);
 
-    const response = await axios.post("http://localhost:3000/form", data);
+      const response = await axios.post("http://localhost:3000/form", data);
 
-    console.log(response);
+      console.log(response);
 
-    alert("Merci pour votre message");
+      alert("Merci pour votre message");
+    } else {
+      alert("Vous devez remplir tous les champs");
+    }
   });
 });
